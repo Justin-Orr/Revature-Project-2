56eeb444d3d4d50e480f2f098df162b067c2034f
Justin <jj.oreo98@gmail.com>
2021-08-10 17:57:22 -0500

    Import data as dataframes

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object App {
 
@@ -12,8 +13,33 @@ object App {
     spark = spark_session_init()
     println("-- Created Spark Session --")
     spark.sparkContext.setLogLevel("ERROR")
-    spark_test()
+    //spark_test()
+    val c19_df = import_data("covid_19_data.csv")
+    c19_df.show(10)
 
+    val c19_confirmed_df = import_data("time_series_covid_19_confirmed.csv")
+    c19_confirmed_df.show(10)
+
+    val c19_confirmed_US_df = import_data("time_series_covid_19_confirmed_US.csv")
+    c19_confirmed_US_df.show(10)
+
+    val c19_deaths_df = import_data("time_series_covid_19_deaths.csv")
+    c19_deaths_df.show(10)
+
+    val c19_deaths_US_df = import_data("time_series_covid_19_deaths_US.csv")
+    c19_deaths_US_df.show(10)
+
+    val c19_recovered_df = import_data("time_series_covid_19_recovered.csv")
+    c19_recovered_df.show(10)
+
+  }
+
+  def import_data(fileName: String):DataFrame = {
+    return spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load("input/" + fileName)
   }
 
   def spark_session_init(): SparkSession = {
@@ -31,4 +57,5 @@ object App {
     spark.sql("SELECT * FROM newone").show()
     spark.sql("DROP table newone")
   }
+
 }
