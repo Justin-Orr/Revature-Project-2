7e6a761811115293c1602f7870d328c2c8394e9d
Justin <jj.oreo98@gmail.com>
2021-08-11 17:16:11 -0500

    Add ability to sum columns for the confirmed covid 19 file by season

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,9 +1,11 @@
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object App {
 
   var spark:SparkSession = null
+  var sc:SparkContext = null
 
   def main(args: Array[String]): Unit = {
     // create a spark session
@@ -11,11 +13,22 @@ object App {
     System.setProperty("hadoop.home.dir", "C:\\winutils")
 
     spark = spark_session_init()
+    sc = spark.sparkContext
     println("-- Created Spark Session --")
     spark.sparkContext.setLogLevel("ERROR")
     //spark_test()
-    Justin.findings()
+    //test(spark)
+    Justin.findings(spark)
 
+  }
+
+  def testToDF(spark:SparkSession): Unit = {
+    //The toDF function only works after you create the spark session and use the following import statement.
+    //The import also has to be in a different method from either main or the function that initializes the spark session
+    import spark.implicits._
+    val rdd1 = sc.parallelize(Seq(1,2))
+    val df = rdd1.toDF()
+    df.show()
   }
 
   def spark_session_init(): SparkSession = {
--- src/main/scala/Justin.scala
+++ src/main/scala/Justin.scala
@@ -1,26 +1,20 @@
-import App.spark
-import org.apache.spark.sql.DataFrame
+import App.{sc, spark}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.col
+
 
 object Justin {
 
-  def findings():Unit = {
-    val c19_df = import_data("covid_19_data.csv")
-    c19_df.show(10)
+  var c19_df:DataFrame = null
+  var confirmed_df:DataFrame = null
+  var confirmed_US_df:DataFrame = null
+  var deaths_df:DataFrame = null
+  var deaths_US_df:DataFrame = null
+  var recovered_df:DataFrame = null
 
-    val c19_confirmed_df = import_data("time_series_covid_19_confirmed.csv")
-    c19_confirmed_df.show(10)
-
-    val c19_confirmed_US_df = import_data("time_series_covid_19_confirmed_US.csv")
-    c19_confirmed_US_df.show(10)
-
-    val c19_deaths_df = import_data("time_series_covid_19_deaths.csv")
-    c19_deaths_df.show(10)
-
-    val c19_deaths_US_df = import_data("time_series_covid_19_deaths_US.csv")
-    c19_deaths_US_df.show(10)
-
-    val c19_recovered_df = import_data("time_series_covid_19_recovered.csv")
-    c19_recovered_df.show(10)
+  def findings(spark:SparkSession):Unit = {
+    create_data_frames()
+    getTotalConfirmationsBySeason(spark)
   }
 
   def import_data(fileName: String):DataFrame = {
@@ -29,6 +23,48 @@ object Justin {
       .option("inferSchema", "true")
       .option("header", "true")
       .load("input/" + fileName)
+  }
+
+  def create_data_frames(): Unit = {
+    //c19_df = import_data("covid_19_data.csv")
+    confirmed_df = import_data("time_series_covid_19_confirmed.csv")
+    //confirmed_US_df = import_data("time_series_covid_19_confirmed_US.csv")
+    //deaths_df = import_data("time_series_covid_19_deaths.csv")
+    //deaths_US_df = import_data("time_series_covid_19_deaths_US.csv")
+    //recovered_df = import_data("time_series_covid_19_recovered.csv")
+  }
+
+  def getTotalConfirmationsBySeason(spark:SparkSession): Unit = {
+    //Create data frames based on all columns within a range based of the column headers of the table
+
+    //Winter Dates 2020
+    val winter_sum_df = get_seasonal_confirmations(spark,4,42,"winter")
+    winter_sum_df.show(10)
+
+  }
+
+  /**
+   * Using the total covid_19 confirmations file, return a df based on the season specified by the range of column header dates.
+   * Look at the file headers to see the dates represented as columns (use excel to quickly grab the indices for each season)
+  */
+  def get_seasonal_confirmations(spark:SparkSession, startIndex:Int, endIndex:Int, season:String):DataFrame = {
+    var colNum = startIndex to endIndex //inclusive
+    val confirmed_season_df = confirmed_df.select(colNum map confirmed_df.columns map col: _*)
+
+    val confirmed_season_rdd = confirmed_season_df.rdd
+    val confirmed_season_2Darray = confirmed_season_rdd.collect()
+    val confirmed_season_array_row = new Array[Int](confirmed_season_2Darray.length)
+
+    for(i <- 0 until confirmed_season_2Darray.length) {
+      var total = 0;
+      for(j <- 0 until confirmed_season_2Darray(i).length) {
+        total = total + confirmed_season_2Darray(i)(j).toString.toInt
+      }
+      confirmed_season_array_row(i) = total
+    }
+
+    import spark.implicits._
+    return sc.parallelize(confirmed_season_array_row).toDF(season + "_total_confirmed")
   }
 
 }
