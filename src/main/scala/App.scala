887bc6a225b9458255ef9f3b7f88f75e21ef27f4
mtorres1127 <87192726+mtorres1127@users.noreply.github.com>
2021-08-13 10:21:51 -0600

    Change 1 (test)

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -38,6 +38,8 @@ object App {
     val df_deaths_US = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_deaths_US.csv")
     val df_recovered = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_recovered.csv")
 
+    //df_covid_19_data.select("Province/State").show()
+
     //CONVERT DATAFRAMES INTO SPARK SQL TABLES
     df_covid_19_data.createOrReplaceTempView("tb_covid_19_data")
     df_confirmed.createOrReplaceTempView("tb_confirmed")
@@ -46,10 +48,27 @@ object App {
     df_deaths_US.createOrReplaceTempView("tb_deaths_US")
     df_recovered.createOrReplaceTempView("tb_recovered")
 
+
     // SPARK SQL QUERIES
-    spark.sql("select * from tb_covid_19_data").show()
-    spark.sql("select sum(Confirmed), sum(Deaths) from tb_covid_19_data group by 'Country/Province' ").show()
-    //spark.sql("select 'Province/State' from tb_covid_19_data where 'Province/State' = 'Wales' ")
+    //spark.sql("update tb_confirmed set `Counrty/Region` = Replace(`Country/Region`,'China','Mainland China')")
+    spark.sql("drop database if exists db_p2 cascade")
+    spark.sql("create database db_p2")
+    spark.sql("use db_p2")
+    spark.sql("select `Country/Region`,sum(Confirmed) as Confirmed,sum(Deaths) as Deaths from tb_covid_19_data group by `Country/Region`")createOrReplaceTempView("tb_5")
+    spark.sql("select * from tb_5").show()
+    spark.sql("SELECT tb_5.`Country/Region`,tb_confirmed.Lat,`Confirmed`,`Deaths` from tb_5 right join tb_confirmed on tb_5.`Country/Region` = tb_confirmed.`Country/Region` order by Lat asc")createOrReplaceTempView("tb_6")
+    spark.sql("select `Country/Region`, lat, confirmed, deaths")
+    //spark.sql("create table tb1 as SELECT tb_covid_19_data.`Province/State`,tb_covid_19_data.`Country/Region`,tb_confirmed.Lat,`Confirmed`,`Deaths`,`Recovered` from tb_covid_19_data right join tb_confirmed on tb_covid_19_data.`Country/Region` = tb_confirmed.`Country/Region` or tb_covid_19_data.`Country/Region` = 'Mainland ' + tb_confirmed.`Country/Region` order by Lat ASC").show()
+   // spark.sql("update tb1 set `Counrty/Region` = Replace(`Country/Region`,'China','Mainland China')")
+    //spark.sql("create table tb2 as SELECT REPLACE('tb1','Mainland China','China'")
+    //spark.sql("select * from tbg7").show()
+    //spark.sql("SELECT tb_covid_19_data.`Province/State`,tb_covid_19_data.`Country/Region`,tb_confirmed.Lat,`Confirmed`,`Deaths`,`Recovered` from tb_covid_19_data right join tb_confirmed on tb_covid_19_data.`Country/Region` = tb_confirmed.`Country/Region` order by Lat ASC")
+    //spark.sql("ALTER TABLE tb1 ADD LatitudeGroup varchar(255)").show()
+    //spark.sql("ALTER TABLE tb_covid_19_data MODIFY COLUMN Lat string")
+    //spark.sql("select * from tb_covid_19_data").show()
+
+    //spark.sql("select `Country/Region`, sum(Confirmed), sum(Deaths) from tb_covid_19_data group by `Country/Region` ").show()
+    //spark.sql("select `Province/State` from tb_covid_19_data").show()
 
     //spark.sql("create table if not exists newone(id Int,name String) row format delimited fields terminated by ','");
     //spark.sql("LOAD DATA LOCAL INPATH 'input/test.txt' INTO TABLE newone")
