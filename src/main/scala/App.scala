b850473385e163400d4ac9b3c353d678b2fa8142
mtorres1127 <87192726+mtorres1127@users.noreply.github.com>
2021-08-18 10:35:21 -0600

    version f.1

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,8 +1,19 @@
-
+/*
+Create a Spark Application that process Covid data.
+Your project  should involve useful analysis of covid data
+(Every concept of spark like rdd, dataframes, sql, dataset and optimization methods  and  persistence should be included).
+The Expected output is different trends that you have observed as part of data collectively and how you can use these trends to make some useful decisions.
+Let the P2, have presentation with screen shots and practical demo.
+ */
+//import Mark.spark
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 object App {
   var spark:SparkSession = null
+  //import spark.implicits._
+  var sc:SparkContext = null
   //var sc = spark.sparkContext;
+  //import spark.implicits._
 
 
   def main(args: Array[String]) : Unit = {
@@ -11,10 +22,12 @@ object App {
     System.setProperty("hadoop.home.dir", "C:\\winutils")
 
     spark = spark_session_init()
+    sc = spark.sparkContext
     println("-- Created Spark Session --")
 
     spark.sparkContext.setLogLevel("ERROR")
-    spark_test()
+    //spark_test()
+    Mark.show_tables(spark)
 
 
   }
@@ -26,45 +39,10 @@ object App {
       .config("spark.master", "local")
       .enableHiveSupport()
       .getOrCreate()
+
   }
 
   def spark_test()  {
-
-    // LOAD DATA INTO DATAFRAMES
-    val df_covid_19_data = spark.read.format("csv").option("header", "true").load("input/covid_19_data.csv")
-    val df_confirmed = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_confirmed.csv")
-    val df_confirmed_US = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_confirmed_US.csv")
-    val df_deaths = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_deaths.csv")
-    val df_deaths_US = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_deaths_US.csv")
-    val df_recovered = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_recovered.csv")
-
-    // CONVERT DATAFRAMES INTO SPARK SQL TABLES
-    df_covid_19_data.createOrReplaceTempView("tb_covid_19_data")
-    df_confirmed.createOrReplaceTempView("tb_confirmed")
-    df_confirmed_US.createOrReplaceTempView("tb_confirmed_US")
-    df_deaths.createOrReplaceTempView("tb_deaths")
-    df_deaths_US.createOrReplaceTempView("tb_deaths_US")
-    df_recovered.createOrReplaceTempView("tb_recovered")
-
-    // SPARK SQL QUERIES TO CREATE LATITUDE TABLE
-    spark.sql("drop database if exists db_p2 cascade")
-    spark.sql("create database db_p2")
-    spark.sql("use db_p2")
-    spark.sql("select `Country/Region`,sum(Confirmed) as Confirmed,sum(Deaths) as Deaths from tb_covid_19_data group by `Country/Region`")createOrReplaceTempView("tb_5")
-    spark.sql("select * from tb_5").show()
-    spark.sql("SELECT tb_5.`Country/Region`,tb_confirmed.Lat,`Confirmed`,`Deaths` from tb_5 right join tb_confirmed on tb_5.`Country/Region` = tb_confirmed.`Country/Region` order by Lat asc")createOrReplaceTempView("tb_6")
-    spark.sql("select `Country/Region`, lat, confirmed, deaths, case when (Lat >= 0 and Lat <= 30) then ' 0  to  30' when (Lat > 30 and Lat <= 60) then ' 30  to  60' when (Lat > 60 and Lat <= 90) then ' 60  to  90' when (Lat < 0 and Lat >= -30) then '-30  to   0' when (Lat < -30 and Lat >= -60) then '-60  to -30' when (Lat < -60 and Lat >= -90) then '-90  to -60' when (`Country/Region` = 'China') then ' 30  to  60' when (`Country/Region` = 'Canada') then ' 30  to  60' else 'unknown' end as Lat_Group from tb_6")createOrReplaceTempView("tb_8")
-    println("Table Data From 1/22/2020 to 5/2/2021")
-    spark.sql("select Lat_Group, round(sum(confirmed)/1000000,2) as Confirmed_Millions, round(sum(deaths)/1000000,2) as Deaths_Millions, concat(cast(round(sum(deaths)/sum(confirmed)*100, 3) as string), ' %')  as Death_Rate from tb_8 group by Lat_Group order by Lat_Group ASC").show()
-
-    // CONVERT TABLE QUERY TO DATAFRAME
-    val df_lat = spark.sql("select Lat_Group, round(sum(confirmed)/1000000,2) as Confirmed_Millions, round(sum(deaths)/1000000,2) as Deaths_Millions, concat(cast(round(sum(deaths)/sum(confirmed)*100, 3) as string), ' %')  as Death_Rate from tb_8 group by Lat_Group order by Lat_Group ASC").show()
-
-    // CONVERT TO RDD
-
-    // CONVERT TO DATASET
-
-    // PERSIST
 
   }
 }
---
+++ src/main/scala/Mark.scala
@@ -0,0 +1,48 @@
+
+import org.apache.spark.sql.{DataFrame,Dataset,Row,SparkSession}
+import App.{sc, spark}
+
+object Mark {
+
+  // LOAD DATA INTO DATAFRAMES
+  val df_covid_19_data = spark.read.format("csv").option("header", "true").load("input/covid_19_data.csv")
+  val df_confirmed = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_confirmed.csv")
+  val df_confirmed_US = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_confirmed_US.csv")
+  val df_deaths = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_deaths.csv")
+  val df_deaths_US = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_deaths_US.csv")
+  val df_recovered = spark.read.format("csv").option("header", "true").load("input/time_series_covid_19_recovered.csv")
+
+  // CONVERT DATAFRAMES INTO SPARK SQL TABLES
+  df_covid_19_data.createOrReplaceTempView("tb_covid_19_data")
+  df_confirmed.createOrReplaceTempView("tb_confirmed")
+  df_confirmed_US.createOrReplaceTempView("tb_confirmed_US")
+  df_deaths.createOrReplaceTempView("tb_deaths")
+  df_deaths_US.createOrReplaceTempView("tb_deaths_US")
+  df_recovered.createOrReplaceTempView("tb_recovered")
+
+
+  def show_tables(spark:SparkSession)= {
+  // SPARK SQL QUERIES TO CREATE LATITUDE TABLE
+  spark.sql("select `Country/Region`,sum(Confirmed) as Confirmed,sum(Deaths) as Deaths from tb_covid_19_data group by `Country/Region`")createOrReplaceTempView("tb_5")
+  spark.sql("select * from tb_5")
+  spark.sql("SELECT tb_5.`Country/Region`,tb_confirmed.Lat,`Confirmed`,`Deaths` from tb_5 right join tb_confirmed on tb_5.`Country/Region` = tb_confirmed.`Country/Region` order by Lat asc")createOrReplaceTempView("tb_6")
+  spark.sql("select `Country/Region`, lat, confirmed, deaths, case when (Lat >= 0 and Lat <= 30) then ' 0  to  30' when (Lat > 30 and Lat <= 60) then ' 30  to  60' when (Lat > 60 and Lat <= 90) then ' 60  to  90' when (Lat < 0 and Lat >= -30) then '-30  to   0' when (Lat < -30 and Lat >= -60) then '-60  to -30' when (Lat < -60 and Lat >= -90) then '-90  to -60' when (`Country/Region` = 'China') then ' 30  to  60' when (`Country/Region` = 'Canada') then ' 30  to  60' else 'unknown' end as Lat_Group from tb_6")createOrReplaceTempView("tb_8")
+  spark.sql("select Lat_Group, round(sum(confirmed)/1000000,2) as Confirmed_Millions, round(sum(deaths)/1000000,2) as Deaths_Millions, concat(cast(round(sum(deaths)/sum(confirmed)*100, 3) as string), ' %')  as Death_Rate from tb_8 group by Lat_Group order by Lat_Group ASC")
+
+  // CONVERT TABLE QUERY TO DATAFRAME
+    val df_lat = spark.sql("select Lat_Group, round(sum(confirmed)/1000000,2) as Confirmed_Millions, round(sum(deaths)/1000000,2) as Deaths_Millions, concat(cast(round(sum(deaths)/sum(confirmed)*100, 3) as string), ' %')  as Death_Rate from tb_8 group by Lat_Group order by Lat_Group ASC")
+    println("Covid Death Rates By Latitude: ")
+    println("(Data From 1/22/2020 to 5/2/2021)")
+    df_lat.show()
+
+  // CONVERT TO RDD
+    val rdd_lat = df_lat.rdd
+    rdd_lat.collect()
+
+  // CONVERT TO DATASET
+    //case class data(Lat_Group: String, Confirmed_Millions: Double, Deaths_Millions: Double, Death_Rate: String)
+    //val ds_lat = spark.createDataset(rdd_lat)
+    //import org.apache.spark.sql.{Encoder, Encoders}
+    //val ds_lat = df_lat.as[data]
+}
+}
